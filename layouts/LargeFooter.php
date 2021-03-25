78cdaed3ef1d8d443ea3673396fe97332d00c465
MahmoudSiliman <modysiliman75@gmail.com>
2021-03-25 13:45:38 +0200

    edit large footer

--- layouts/LargeFooter.php
+++ layouts/LargeFooter.php
@@ -4,13 +4,7 @@
     <div class="row">
       <div class="col-12 col-md-6 p-0">
         <div class="map_wraper">
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19338.15820346373!2d31.374660789535696!3d31.039183138529495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sar!2seg!4v1616017063461!5m2!1sar!2seg" 
-            style="border:0;" 
-            allowfullscreen="" 
-            loading="lazy"
-          >
-          </iframe>
+          <img src="assets/media/pic18.jpg">
         </div>
       </div>
 
